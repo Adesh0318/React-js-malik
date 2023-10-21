b53ea6f77a4a9604b1dec2ee4c903c22b24ef9ab
Adesh0318 <adesh.singh.malik.212@gmail.com>
2023-10-21 18:43:27 +0530

    second class

---
+++ Adesh Malik/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>Hello this is the vite and react project </>
+  )
+}
+
+export default App
---
+++ basic/src/Welcome.js
@@ -0,0 +1,6 @@
+function Welcome(){
+    return(
+        <h2>Hello this is the Welcome page Component</h2>
+    )
+}
+export default Welcome
